9634c2fd0a91361cacdb3e49c4ed2f6385552348
Player939 <nielsenrobert492@gmail.com>
2025-12-14 22:34:05 -0700

    Create firebase.js
    Add Firebase Initializer.

---
+++ js/firebase.js
@@ -0,0 +1,14 @@
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  const firebaseConfig = {
+    apiKey: "AIzaSyDyiDXH15yU9x2yWcNdbxerynTacJlHlKU",
+    authDomain: "learning-space-game.firebaseapp.com",
+    projectId: "learning-space-game",
+    storageBucket: "learning-space-game.firebasestorage.app",
+    messagingSenderId: "591066624140",
+    appId: "1:591066624140:web:c0f3253680fef8e2680ba9"
+  };
+
+  // Initialize Firebase
+export const app = initializeApp(firebaseConfig);
