3d6f8a9766b53470ad69daf8dcbe04fa3e25323e
Player939 <nielsenrobert492@gmail.com>
2025-12-14 22:26:23 -0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,7 @@
+// main.js
+import "./firebase.js";   // loads firebase config
+import "./saves.js";      // Google Sheets sync
+import "./menu.js";       // menu logic
+import "./game.js";       // main game logic
 
+console.log("Game scripts loaded!")
