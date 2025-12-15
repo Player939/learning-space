3509cb6e57d7cd4a8c0af022c52ff2f80f2d4a84
Player939 <nielsenrobert492@gmail.com>
2025-12-14 22:55:36 -0700

    Update saves.js

--- js/saves.js
+++ js/saves.js
@@ -1,1 +1,37 @@
 
+  // ---------- GOOGLE SHEETS ----------
+  const SHEET_URL = 'https://script.google.com/macros/s/AKfycbwct-j94qIwoqet8fc3uss54WZ0uK2EsBpQSUkcb6N5nOtHofouSSsldnVZiaJ7XKlvOw/exec';
+
+  export async function syncLoad() {
+    try {
+      const res = await fetch(SHEET_URL + '?action=getData');
+      const data = await res.json();
+      data.forEach(([p, h, s]) => {
+        if (p === 'Player1') { highScore1 = h; step1 = s; }
+        if (p === 'Player2') { highScore2 = h; step2 = s; }
+      });
+      applyMenuValues();
+
+    } catch {
+      step1 = Number(localStorage.getItem('player1Speed')) || step1;
+      step2 = Number(localStorage.getItem('player2Speed')) || step2;
+      applyMenuValues();
+
+    }
+  }
+
+  export async function syncSave() {
+    try {
+      fetch(`${SHEET_URL}?action=setData&player=Player1&highScore=${highScore1}&speed=${step1}`);
+      fetch(`${SHEET_URL}?action=setData&player=Player2&highScore=${highScore2}&speed=${step2}`);
+      localStorage.setItem('player1Speed', step1);
+      localStorage.setItem('player2Speed', step2);
+      localStorage.setItem('highScore1', highScore1);
+      localStorage.setItem('highScore2', highScore2);
+    } catch {
+      localStorage.setItem('player1Speed', step1);
+      localStorage.setItem('player2Speed', step2);
+      localStorage.setItem('highScore1', highScore1);
+      localStorage.setItem('highScore2', highScore2);
+    }
+  }
